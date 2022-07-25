3a9f77613e9d15dbcce5c041d96e8b612615a384
ojsj18 <juhnior88@gmail.com>
2022-07-25 19:10:28 -0300

    doing

--- professor/AR-fontesRuby/Has_many/criaTudo.sh
+++
@@ -1,9 +0,0 @@
-rm -f Tabelas.sqlite3
-ruby criaEstados.rb 
-ruby criaPessoas.rb
-ruby criaDocumentos.rb
-ruby criaMunicipios.rb 
-
-ruby populaEstados.rb
-ruby populaPessoasD.rb
-ruby populaMunicipos.rb
